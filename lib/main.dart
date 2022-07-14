2032dd73d44df405e5497e0b97d59c4198f6ee4f
MoodYBusaidi <moody.busaidi99@gmail.com>
2022-07-14 13:00:29 +0400

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import './transaction.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: "t1",
+      title: 'New Shoes',
+      amount: 19.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "t2",
+      title: 'petrol',
+      amount: 10,
+      date: DateTime.now(),
+    ),
+  ];
+  // late String titleInput;
+  // late String amountInput;
+  final titleController = TextEditingController();
+  final amountContoller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('CHART!'),
+            ),
+          ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextField(
+                    decoration: const InputDecoration(labelText: 'Title'),
+                    controller: titleController,
+                    // onChanged: (val) {
+                    //   titleInput = val;
+                    // },
+                  ),
+                  TextField(
+                    decoration: const InputDecoration(labelText: 'Amount'),
+                    controller: amountContoller,
+                    // onChanged: (val) => amountInput = val,
+                  ),
+                  FlatButton(
+                    child: const Text('Add Transaction'),
+                    textColor: Colors.purple,
+                    onPressed: () {
+                      print(titleController.text);
+                      print(amountContoller.text);
+                    },
+                  )
+                ],
+              ),
+            ),
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                  child: Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Text(
+                      '\$ ${tx.amount}',
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMd().format(tx.date),
+                        style: const TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+            }).toList(),
+          )
+        ],
+      ),
+    );
+  }
+}
